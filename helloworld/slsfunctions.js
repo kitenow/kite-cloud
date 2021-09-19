2c5df98dc75077dee65e4229bedaafb7b35307d4
kitenow <kitenow@google.com>
2021-09-19 17:48:49 +0900

    split-stack-V1

---
+++ helloworld/slsfunctions.js
@@ -0,0 +1,13 @@
+const yamljs = require('yamljs')
+const fs = require('fs')
+module.exports.functions = function() {    
+    let ymlfiles = fs.readdirSync('./ymls')
+    ymlfiles = ymlfiles.filter(file => file.endsWith('yml'))
+    ymlfiles = ymlfiles.map(file => `./ymls/${file}`)
+    console.log(ymlfiles)
+
+    let fns = ymlfiles.map(path => fs.readFileSync(path, 'utf8'))
+    fns = fns.map(raw => yamljs.parse(raw).functions)
+    fns = fns.reduce((result, functions) => {return Object.assign(result, functions)}, {})
+    return fns
+}
