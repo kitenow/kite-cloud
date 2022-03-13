5eff0a854f8e456e823b38f6a953fa9f1337dd0d
Kite <avg1206@gmail.com>
2022-03-13 22:30:06 +0900

    sls manual routing

--- hwv1/handler.js
+++ hwv1/handler.js
@@ -1,8 +1,53 @@
-"use strict";
+'use strict';
 
-module.exports.hello = async (event) => {
+function get(userId) {
   return {
-    statusCode: 200,
-    body: 'Hello from Lambda Serverless V1'
+    body: { id: userId, name: "test" }
   };
+}
+
+function post(userId, header, body) {
+  return {
+    body: { id: userId, header: header, body: body }
+  };
+}
+
+const routeMap = {
+  '/': {
+    'GET': (event, context) => {
+      let  result = {"event" : event, "context" : context}
+      return result;
+    }
+  },
+  '/hello/{userId}': {
+    'GET': (event, context) => {
+      const userId = event.params.path.userId;
+      return get(userId);
+    },
+    'POST': (event, context) => {
+      const userId = event.params.path.userId;
+      const body = JSON.stringify(event["body-json"]);
+      const header =  event.params.header;
+      return post(userId, header, body);
+    }
+  }
+};
+
+function router(event, context) {
+  const controller = routeMap[event.context["resource-path"]][event.context["http-method"]];
+
+  // https://devstarsj.github.io/cloud/2016/11/22/Lambda+APIGateway.02.Route/
+
+  if(!controller) {
+    return {
+      body: { Error: "Invalid Path" }
+    };
+  }
+
+  return controller(event, context);
+}
+
+module.exports.hello = async (event, context, callback) => {
+  let result = router(event, context);
+  callback(null, result);
 };
